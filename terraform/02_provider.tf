31f4301a29d27dba5d740d4f7d83d324a487bd05
jro <j@ro.vu>
2020-12-25 15:36:29 -0600

    Terraform: Adding support for remote backend (Terraform Cloud), for usage in GitHub Actions

--- terraform/02_provider.tf
+++ terraform/02_provider.tf
@@ -2,6 +2,27 @@
 # Provider and access details
 #############################
 
+# Terraform backend
+# See: https://learn.hashicorp.com/tutorials/terraform/github-actions
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+
+  backend "remote" {
+    organization = "jro"
+
+    workspaces {
+      name = "quote-service"
+    }
+  }
+}
+
+
+
+
 provider "aws" {
   shared_credentials_file = "$HOME/.aws/credentials"
   profile                 = "default"
